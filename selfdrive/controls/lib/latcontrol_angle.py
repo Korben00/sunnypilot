bbd1ba29228ad2a314f6bd8539682a49348a394b
DevTekVE <devtekve@gmail.com>
2025-03-09 17:00:14 +0100

    Significant improvement on the jerkiness
    (cherry picked from commit f017954027c52dec6ec1e0b9a7d0d7c0f4dc03c8)

--- selfdrive/controls/lib/latcontrol_angle.py
+++ selfdrive/controls/lib/latcontrol_angle.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 from cereal import log
 from openpilot.selfdrive.controls.lib.latcontrol import LatControl
@@ -6,6 +7,9 @@ from openpilot.selfdrive.controls.lib.latcontrol import LatControl
 # TODO This is speed dependent
 STEER_ANGLE_SATURATION_THRESHOLD = 2.5  # Degrees
 
+# Define a speed-based scaling factor similar to low_speed_factor
+LOW_SPEED_X = [0, 10, 20, 30]   # Speed breakpoints
+LOW_SPEED_Y = [0.67, 0.77, 0.91, 1.0]  # Factor reducing influence at low speeds
 
 class LatControlAngle(LatControl):
   def __init__(self, CP, CP_SP, CI):
@@ -21,8 +25,14 @@ class LatControlAngle(LatControl):
       angle_steers_des = float(CS.steeringAngleDeg)
     else:
       angle_log.active = True
-      angle_steers_des = math.degrees(VM.get_steer_from_curvature(-desired_curvature, CS.vEgo, params.roll))
-      angle_steers_des += params.angleOffsetDeg
+
+      # Compute the base desired steering angle
+      base_angle_steers_des = math.degrees(VM.get_steer_from_curvature(-desired_curvature, CS.vEgo, params.roll))
+      base_angle_steers_des += params.angleOffsetDeg
+
+      # Apply a low-speed factor to reduce aggressive changes at low speeds
+      low_speed_factor = np.interp(CS.vEgo, LOW_SPEED_X, LOW_SPEED_Y)
+      angle_steers_des = CS.steeringAngleDeg + low_speed_factor * (base_angle_steers_des - CS.steeringAngleDeg)
 
     if self.use_steer_limited_by_safety:
       # these cars' carcontrollers calculate max lateral accel and jerk, so we can rely on carOutput for saturation
@@ -33,5 +43,6 @@ class LatControlAngle(LatControl):
       angle_control_saturated = abs(angle_steers_des - CS.steeringAngleDeg) > STEER_ANGLE_SATURATION_THRESHOLD
     angle_log.saturated = bool(self._check_saturation(angle_control_saturated, CS, False, curvature_limited))
     angle_log.steeringAngleDeg = float(CS.steeringAngleDeg)
-    angle_log.steeringAngleDesiredDeg = angle_steers_des
+    angle_log.steeringAngleDesiredDeg = float(angle_steers_des)
+
     return 0, float(angle_steers_des), angle_log
